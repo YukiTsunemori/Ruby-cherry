d425e07423ab7882271d8551495e6c27a461e8bc
YukiTsunemori <yuki.tsunemori1229@gmail.com>
2025-02-13 04:19:13 +0000

    A

--- foo.rb
+++ foo.rb
@@ -8,5 +8,22 @@ class Bar < Foo
     #クラスを継承するとクラスメソッド（ここではself.hello）も継承されるので、呼び出せる
 end
 
-p Foo.hello
-p Bar.hello
+# p Foo.hello
+# p Bar.hello
+
+class Product
+    private
+    def name
+        p 'this is private'
+    end
+end
+
+class DVD < Product
+    def to_s
+        p "name: #{name}"
+    end
+end
+
+# dvd = DVD.new
+# dvd.to_s
+
--- user.rb
+++ user.rb
@@ -24,5 +24,34 @@ class User2
 end
 
 # publicはクラスの外部からでも自由に呼び出せるメソッド。
-user = User2.new
-p user.hello
+# user = User2.new
+# p user.hello
+
+class User3
+    def foo
+        'foo'
+    end
+
+    def bar
+        'bar'
+    end
+
+    # privateキーワードはメソッドなので、privateにしたいメソッドを引数に渡すことができる。
+    # 以下のメソッドはprivateにはならない。
+    private :foo, :bar
+
+    def baz
+        'baz'
+    end
+    # メソッドの戻り値はシンボルでメソッド名が返るので、そのまま引数としてprivateに渡すことができる。
+    # private def baz
+    #         'baz'
+    # end
+
+end
+
+user3 = User3.new
+# p user3.foo
+# p user3.bar
+p user3.baz
+
